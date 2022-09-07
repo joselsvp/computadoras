3ef8e94d8e2665a6ac5c65d292970c2cc0e3a922
Jose Villanueva <jose.luis@grupoicarus.com.mx>
2022-09-07 18:50:42 -0500

    update

---
+++ config/database_config.php
@@ -0,0 +1,8 @@
+<?php
+$config_values = array(
+    'default' => array(
+        'host' => 'mysql:host=127.0.0.1:3306;dbname=computadoras;charset=utf8',
+        'user' => 'root',
+        'password' => ''
+    )
+);

fb8e5d48326b6a9a7717e276363727ffa07afb08
Jose Villanueva <jose.luis@grupoicarus.com.mx>
2022-09-07 11:36:59 -0500

    update: styles icon

--- views/productoIndex.php
+++ views/productoIndex.php
@@ -16,6 +16,8 @@ require_once 'sidebar.php'
                 foreach ($getProducts as $key => $product){ ?>
                     <a href="<?=base_url?>producto/show?id=<?=base64_encode($product['id'])?>" title="Ver <?= $product['modelo'] ?>">
                         <div class="card">
+                            <div class="product-famous"><i class="fas fa-star"></i> Producto destacado</div>
+
                             <div class="card-header" style="text-align: center">
                                 <img src="<?= $product['url_image'] ?>" alt="<?= $product['modelo'] ?>" style="background-position: center; background-size: cover; background-repeat: no-repeat; background-attachment: fixed; width: 200px">
                             </div>
